88c0c023fef8c91d880ef985c81f520e6069314c
vkamsingh <112477955+vkamsingh@users.noreply.github.com>
2022-11-09 10:21:43 +0530

    Update Player.js

--- src/Player.js
+++ src/Player.js
@@ -1,249 +1,224 @@
-const Queue = require("./Queue");
-const FerraLink = require("./FerraLink");
-const shoukaku = require("shoukaku");
+const Queue = require('./Queue');
+const FerraLink = require('./FerraLink');
+const shoukaku = require('shoukaku');
 
 class Player {
-  /**
-   *
-   * @param {FerraLink} manager
-   * @param {PlayerOptions} options
-   */
-  constructor(manager, options) {
-    /** @type {FerraLink} */
-    this.manager = manager;
+	/**
+	 *
+	 * @param {FerraLink} manager
+	 * @param {PlayerOptions} options
+	 */
+	constructor(manager, options) {
+		/** @type {FerraLink} */
+		this.manager = manager;
 
-    /** @type {string} */
-    this.guildId = options.guildId;
+		/** @type {string} */
+		this.guildId = options.guildId;
 
-    /** @type {string} */
-    this.voiceId = options.voiceId;
+		/** @type {string} */
+		this.voiceId = options.voiceId;
 
-    /** @type {string} */
-    this.textId = options.textId;
+		/** @type {string} */
+		this.textId = options.textId;
 
-    /** @type {number} */
-    this.volume = options.volume;
+		/** @type {number} */
+		this.volume = options.volume;
 
-    /** @type {shoukaku.Player} */
-    this.shoukaku = options.ShoukakuPlayer;
+		/** @type {shoukaku.Player} */
+		this.shoukaku = options.ShoukakuPlayer;
 
-    /** @type {Queue} */
-    this.queue = new Queue();
+		/** @type {Queue} */
+		this.queue = new Queue();
 
-    /** @type {boolean} */
-    this.paused = false;
+		/** @type {boolean} */
+		this.paused = false;
 
-    /** @type {boolean} */
-    this.playing = false;
+		/** @type {boolean} */
+		this.playing = false;
 
-    /** @type {Map<string|number, any>} */
-    this.data = new Map();
+		/** @type {Data} */
+		this.data = new Map();
 
-    /** @type {LoopType} */
-    this.loop = "none";
+		/** @type {LoopType} */
+		this.loop = 'none';
 
-    const player = this.shoukaku;
-    player.on("start", () => {
-      this.playing = true;
-      this.manager.emit("trackStart", this, this.queue.current);
-    });
-    player.on("end", (data) => {
-      if (data.reason === "REPLACED")
-        return this.manager.emit("trackEnd", this);
-      if (["LOAD_FAILED", "CLEAN_UP"].includes(data.reason)) {
-        this.queue.previous = this.queue.current;
-        this.playing = false;
-        this.manager.emit("trackEnd", this, this.queue.current);
-        this.queue.current = null;
-        return this.play();
-      }
-      const current = this.queue.current;
-      this.queue.previous = this.queue.current;
-      this.queue.current = null;
-      if (this.loop === "track") this.queue.unshift(this.queue.previous);
-      if (this.loop === "queue") this.queue.push(this.queue.previous);
+		const player = this.shoukaku;
+		player.on('start', () => {
+			this.playing = true;
+			this.manager.emit('trackStart', this, this.queue.current);
+		});
+		player.on('end', data => {
+			if (data.reason === 'REPLACED') return this.manager.emit('trackEnd', this);
+			if (['LOAD_FAILED', 'CLEAN_UP'].includes(data.reason)) {
+				this.queue.previous = this.queue.current;
+				this.playing = false;
+				this.manager.emit('trackEnd', this, this.queue.current);
+				this.queue.current = null;
+				return this.play();
+			}
+			if (this.loop === 'track' && this.queue.current) this.queue.unshift(this.queue.current);
+			if (this.loop === 'queue' && this.queue.current) this.queue.push(this.queue.current);
 
-      if (this.queue.length) this.manager.emit("trackEnd", this, current);
-      else {
-        this.playing = false;
-        return this.manager.emit("queueEnd", this);
-      }
-      this.play();
-    });
-    player.on("closed", (data = WebSocketClosedEvent) => {
-      this.playing = false;
-      this.manager.emit("PlayerClosed", this, data);
-    });
-    player.on("exception", (data = TrackExceptionEvent) => {
-      this.playing = false;
-      this.manager.emit("trackException", this, data);
-    });
-    player.on("update", (data = PlayerUpdate) =>
-      this.manager.emit("PlayerUpdate", this, data)
-    );
-    player.on("stuck", (data = TrackStuckEvent) =>
-      this.manager.emit("trackStuck", this, data)
-    );
-    player.on("resumed", () => this.manager.emit("PlayerResumed", this));
-  }
+			this.queue.previous = this.queue.current;
+			const current = this.queue.current;
+			this.queue.current = null;
 
-  /**
-   * Check if player exists
-   * @returns {boolean}
-   */
-  get exists() {
-    return this.manager.players.has(this.guildId);
-  }
+			if (this.queue.length) this.manager.emit('trackEnd', this, current);
+			else {
+				this.playing = false;
+				return this.manager.emit('queueEnd', this);
+			}
+			this.play();
+		});
+		player.on('closed', (data = WebSocketClosedEvent) => {
+			this.playing = false;
+			this.manager.emit('PlayerClosed', this, data);
+		});
+		player.on('exception', (data = TrackExceptionEvent) => {
+			this.playing = false;
+			this.manager.emit('trackException', this, data);
+		});
+		player.on('update', (data = PlayerUpdate) => this.manager.emit('PlayerUpdate', this, data));
+		player.on('stuck', (data = TrackStuckEvent) => this.manager.emit('trackStuck', this, data));
+		player.on('resumed', () => this.manager.emit('PlayerResumed', this));
+	}
 
-  /**
-   * Play the queue
-   * @returns {Promise<void>}
-   */
-  async play() {
-    if (!this.exists || !this.queue.length) return;
-    let newTrack = this.queue.shift();
-    if (!newTrack.track) newTrack = await this.resolve(newTrack);
-    this.queue.current = newTrack;
+	/**
+     * Pause or resume the player
+     * @param {boolean} [pause]
+     * @returns {Player}
+     */
+	pause(pause = true) {
+		if (typeof pause !== 'boolean') throw new RangeError('[FerraLink] => Pause function must be pass with boolean value.');
+		if (this.paused === pause || !this.queue.totalSize) return this;
+		this.paused = pause;
+		this.playing = !pause;
+		this.shoukaku.setPaused(pause);
+		return this;
+	}
 
-    const playOptions = { noReplace: false };
-    this.shoukaku
-      .playTrack({ track: this.queue.current.track }, playOptions)
-      .setVolume(this.volume / 100);
-  }
+	/**
+	 * Skip the current track
+	 * @returns {Player}
+	 */
+	skip() {
+		this.shoukaku.stopTrack();
+		return this;
+	}
 
-  /**
-   * Resolve a track
-   * @param {shoukaku.Track} track
-   * @returns {Promise<shoukaku.Track>}
-   */
-  async resolve(track) {
-    const query = [track.info.author, track.info.title]
-      .filter((x) => !!x)
-      .join(" - ");
-    let result = await this.shoukaku.node.rest.resolve(`ytmsearch:${query}`);
-    if (!result || !result.tracks.length) {
-      result = await this.shoukaku.node.rest.resolve(`ytsearch:${query}`);
-      if (!result || !result.tracks.length) return;
-    }
-    track.track = result.tracks[0].track;
-    return track;
-  }
+	/**
+	 * Seek to specific time
+	 * @param {number} position time in ms
+	 * @returns {Player}
+	 */
+	seekTo(position) {
+		if (Number.isNaN(position)) throw new RangeError('[FerraLink] => seek Position must be a number.');
+		this.shoukaku.seekTo(position);
+		return this;
+	}
 
-  /**
-   * Pause or resume the player
-   * @param {boolean} [pause]
-   * @returns {Player}
-   */
-  pause(pause = true) {
-    if (typeof pause !== "boolean")
-      throw new RangeError(
-        "[FerraLink] => Pause function must be pass with boolean value."
-      );
-    if (this.paused === pause || !this.queue.totalSize) return this;
-    this.paused = pause;
-    this.playing = !pause;
-    this.shoukaku.setPaused(pause);
-    return this;
-  }
+	/**
+	 * Set the volume
+	 * @param {number} volume
+	 * @returns {Player}
+	 */
+	setVolume(volume) {
+		if (Number.isNaN(volume)) throw new RangeError('[FerraLink] => Volume level must be a number.');
+		this.shoukaku.setVolume(volume / 100);
+		this.volume = volume;
+		return this;
+	}
 
-  /**
-   * Skip the current track
-   * @returns {Player}
-   */
-  skip() {
-    this.shoukaku.stopTrack();
-    return this;
-  }
+	/**
+	 * Change player's text channel
+	 * @param {string} textId
+	 * @returns {Player}
+	 */
+	setTextChannel(textId) {
+		if (typeof textId !== 'string') throw new RangeError('[FerraLink] => textId must be a string.');
+		this.textId = textId;
+		return this;
+	}
 
-  /**
-   * Seek to specific time
-   * @param {number} position time in ms
-   * @returns {Player}
-   */
-  seekTo(position) {
-    if (Number.isNaN(position))
-      throw new RangeError("[FerraLink] => seek Position must be a number.");
-    this.shoukaku.seekTo(position);
-    return this;
-  }
+	/**
+	 * Change player's voice channel
+	 * @param {string} voiceId
+	 * @returns {Player}
+	 */
+	setVoiceChannel(voiceId) {
+		if (typeof voiceId !== 'string') throw new RangeError('[FerraLink] => voiceId must be a string.');
+		this.voiceId = voiceId;
+		return this;
+	}
 
-  /**
-   * Set the volume
-   * @param {number} volume
-   * @returns {Player}
-   */
-  setVolume(volume) {
-    if (Number.isNaN(volume))
-      throw new RangeError("[FerraLink] => Volume level must be a number.");
-    this.shoukaku.setVolume(volume / 100);
-    this.volume = volume;
-    return this;
-  }
+	/**
+	 * Change the player's loop mode
+	 * @param {LoopType} method
+	 * @returns {Player}
+	 */
+	setLoop(method) {
+		if (!method) throw new Error('[FerraLink] => You must have to provide loop method as argument for setLoop.');
+		if (method === 'track' || method === 'queue') {
+			this.loop = method;
+			return this;
+		}
+		this.loop = 'none';
+		return this;
+	}
 
-  /**
-   * Change player's text channel
-   * @param {string} textId
-   * @returns {Player}
-   */
-  setTextChannel(textId) {
-    if (typeof textId !== "string")
-      throw new RangeError("[FerraLink] => textId must be a string.");
-    this.textId = textId;
-    return this;
-  }
+	/**
+	 * Play the queue
+	 * @returns {Promise<void>}
+	 */
+	async play() {
+		let Track = this.queue.shift();
+		if (!Track.track) Track = await this.resolve(Track);
+		this.queue.current = Track;
 
-  /**
-   * Change player's voice channel
-   * @param {string} voiceId
-   * @returns {Player}
-   */
-  setVoiceChannel(voiceId) {
-    if (typeof voiceId !== "string")
-      throw new RangeError("[FerraLink] => voiceId must be a string.");
-    this.voiceId = voiceId;
-    return this;
-  }
+		const playOptions = { noReplace: false };
+		this.shoukaku
+			.playTrack({ track: this.queue.current.track }, playOptions)
+			.setVolume(this.volume / 100);
+	}
 
-  /**
-   * Change the player's loop mode
-   * @param {LoopType} method
-   * @returns {Player}
-   */
-  setLoop(method) {
-    if (!method)
-      throw new Error(
-        "[FerraLink] => You must have to provide loop method as argument for setLoop."
-      );
-    if (method === "track" || method === "queue") {
-      this.loop = method;
-      return this;
-    }
-    this.loop = "none";
-    return this;
-  }
+	/**
+	 * Resolve a track
+	 * @param {shoukaku.Track} track
+	 * @returns {Promise<shoukaku.Track>}
+	 */
+	async resolve(track) {
+		const query = [track.info.author, track.info.title].filter(x => !!x).join(' - ');
+		let result = await this.shoukaku.node.rest.resolve(`ytmsearch:${query}`);
+		if (!result || !result.tracks.length) {
+			result = await this.shoukaku.node.rest.resolve(`ytsearch:${query}`);
+			if (!result || !result.tracks.length) return;
+		}
+		track.track = result.tracks[0].track;
+		return track;
+	}
 
-  /**
-   * Disconnect the player
-   * @returns {void}
-   */
-  disconnect() {
-    this.pause(true);
-    this.voiceId = null;
-    this.queue.current = null;
-    this.queue.clear();
-  }
+	/**
+	 * Disconnect the player
+	 * @returns {void}
+	 */
+	disconnect() {
+		this.pause(true);
+		this.voiceId = null;
+		this.queue.current = null;
+		this.queue.clear();
+	}
 
-  /**
-   * Destroy the player
-   * @returns {void}
-   */
-  destroy() {
-    this.disconnect();
-    this.shoukaku.connection.disconnect();
-    this.shoukaku.removeAllListeners();
-    this.manager.emit("playerDestroy", this);
-    this.manager.players.delete(this.guildId);
-  }
+	/**
+	 * Destroy the player
+	 * @returns {void}
+	 */
+	destroy() {
+		this.disconnect();
+		this.shoukaku.connection.disconnect();
+		this.shoukaku.removeAllListeners();
+		this.manager.emit('playerDestroy', this);
+		this.manager.players.delete(this.guildId);
+	}
 }
 
 module.exports = Player;
